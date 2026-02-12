50366becc2a806f5164e5332c782907b843a2445
Syed Danial Asghar Zaidi <56907064+Aghadani@users.noreply.github.com>
2026-02-13 00:58:23 +0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -54,7 +54,8 @@
         .academy-center-title {
             position: absolute;
             left: 50%;
-            transform: translateX(-50%);
+            /* Changed from -50% to -60% to nudge it slightly left */
+            transform: translateX(-60%); 
             color: #000;
             font-weight: 800;
             font-size: 1.6rem;
@@ -62,6 +63,7 @@
             text-transform: uppercase;
             margin: 0;
             text-shadow: 1px 1px 0px #fff, 2px 2px 0px rgba(0,0,0,0.1);
+            white-space: nowrap; /* Prevents text from wrapping */
         }
 
         /* Nav Links */
@@ -115,7 +117,7 @@
     <div class="container-fluid px-4">
         <a class="navbar-brand d-flex align-items-center" href="dashboard.php">
             <img src="../uploads/Logo_Academy.png" class="header-logo" alt="Logo">
-            <span style="color: #000; font-weight: 800; letter-spacing: -1px;">ADmin Pannel</span>
+            <span style="color: #000; font-weight: 800; letter-spacing: -1px;">Admin Pannel</span>
         </a>
 
         <div class="academy-center-title d-none d-lg-block">
